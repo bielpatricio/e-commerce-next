dc3259358e2935a3778f82798a691af6b02cf94d
Gabriel Patrício <gabrieltp087@gmail.com>
2023-12-01 10:58:38 -0300

    first commit

---
+++ src/app/(store)/(home)/loading.tsx
@@ -0,0 +1,7 @@
+export default function HomeLoading() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-zinc-950">
+      <h1 className="mt-4 text-2xl font-bold text-white">Loading...</h1>
+    </div>
+  )
+}
